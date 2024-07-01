9d5f32c1ee3befb55e60dd9234476df9f2dbe03c
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-01 13:21:52 +0600

    trying to open select-option by clicking angel down icon

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -2,8 +2,25 @@ import bannerImg from "../../../assets/banner/banner-image.png";
 import "./Banner.css";
 import locationIcon from "../../../assets/icons/location-icon.svg";
 import angelDown from "../../../assets/icons/angel-down.svg";
+import { useRef } from "react";
 
 const Banner = () => {
+  const selectRef1 = useRef(null);
+  const selectRef2 = useRef(null);
+
+  const handleImageClick = (ref) => {
+    if (ref.current) {
+      ref.current.focus();
+      const event = new MouseEvent('mousedown', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+      });
+      ref.current.dispatchEvent(event);
+    }
+  };
+
+
   return (
     <div className="bg-[#086060] banner relative">
       <div className="w-[1589px] mx-auto flex">
@@ -28,7 +45,10 @@ const Banner = () => {
             </div>
             <div className="border-b border-[#808080] flex justify-between pt-[48px] px-[32px] pb-[30px] gap-[100px]">
               <div className="w-full relative">
-                <select className="select w-full text-2xl text-[#808080]">
+                <select
+                  ref={selectRef1}
+                  className="select w-full text-2xl text-[#808080]"
+                >
                   <option disabled selected>
                     Symptoms
                   </option>
@@ -37,13 +57,17 @@ const Banner = () => {
                   <option>React</option>
                 </select>
                 <img
-                  className="absolute right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer"
+                  className="absolute right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer z-20"
                   src={angelDown}
                   alt="angel down"
+                  onClick={() => handleImageClick(selectRef1)}
                 />
               </div>
               <div className="w-full relative">
-                <select className="select w-full text-2xl text-[#808080]">
+                <select
+                  ref={selectRef2}
+                  className="select w-full text-2xl text-[#808080]"
+                >
                   <option disabled selected>
                     Diseases
                   </option>
@@ -52,9 +76,10 @@ const Banner = () => {
                   <option>React</option>
                 </select>
                 <img
-                  className="absolute right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer"
+                  className="absolute right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer z-20"
                   src={angelDown}
                   alt="angel down"
+                  onClick={() => handleImageClick(selectRef2)}
                 />
               </div>
             </div>
