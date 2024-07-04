7498b2fcba0266d6bbdb1426aa234a5853c23e71
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-04 19:50:26 +0600

    created navbar with preline ui

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,75 +1,105 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import navLogo from "../../../assets/navbar/nav-logo.svg";
 import "./Navbar.css";
 
 const Navbar = () => {
-  const navLinks = (
-    <>
-      <li>
-        <NavLink to="/doctors">Doctors</NavLink>
-      </li>
-      <li>
-        <NavLink to="/consult">Consult Online</NavLink>
-      </li>
-      <li>
-        <NavLink to="/hospitals">Hospitals</NavLink>
-      </li>
-      <li>
-        <NavLink to="/labTests">Lab Tests</NavLink>
-      </li>
-      <li>
-        <NavLink to="/Surgeries">Surgeries</NavLink>
-      </li>
-      <li>
-        <NavLink to="/healthBlog">Health Blogs</NavLink>
-      </li>
-    </>
-  );
-
   return (
-    <div className="w-[1589px] mx-auto navbar flex justify-between">
-      {/* nav left */}
-      <div className=" ">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            {navLinks}
-          </ul>
-        </div>
-        {/* navbar logo */}
-        <div>
+    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap w-[1589px] mx-auto bg-white py-[22px]">
+      <nav
+        className="w-full mx-auto px-4 flex flex-wrap basis-full items-center justify-between"
+        aria-label="Global"
+      >
+        <Link
+          to="/"
+          className="sm:order-1 flex-none text-xl font-semibold dark:text-white"
+        >
           <img src={navLogo} alt="logo of navbar" />
+        </Link>
+        <div className="sm:order-3 flex items-center gap-x-2">
+          <button
+            type="button"
+            className="sm:hidden hs-collapse-toggle p-2.5 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
+            data-hs-collapse="#navbar-alignment"
+            aria-controls="navbar-alignment"
+            aria-label="Toggle navigation"
+          >
+            <svg
+              className="hs-collapse-open:hidden flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+            <svg
+              className="hs-collapse-open:block hidden flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </button>
+          {/* button div */}
+          <div className="flex items-center gap-[17px]">
+            <Link to="/login">
+              <p className="text-white font-nunito text-xl font-bold bg-[#086060] rounded-[21px]  py-[18px] px-[40px]">
+                Login
+              </p>
+            </Link>
+            <Link to="/signUp">
+              <button className="text-white font-nunito text-xl font-bold px-[40px] py-[18px] bg-[#3BA4A7] rounded-[21px]">
+                SignUp
+              </button>
+            </Link>
+          </div>
         </div>
-      </div>
-      {/* nav middle */}
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{navLinks}</ul>
-      </div>
-      {/* nav right */}
-      <div className="">
-        <button className="loginBtn">Login</button>
-        <button className="singUpBtn">SignUp</button>
-      </div>
-    </div>
+        <div
+          id="navbar-alignment"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:grow-0 sm:basis-auto sm:block sm:order-2"
+        >
+          <div className="flex flex-col gap-[60px] mt-5 sm:flex-row sm:items-center sm:mt-0 sm:ps-5">
+            <NavLink to="/doctors" className="" aria-current="page">
+              Doctors
+            </NavLink>
+            <NavLink to="/consultOnline" className="">
+              Consult Online
+            </NavLink>
+            <NavLink to="/hospitals" className="">
+              Hospitals
+            </NavLink>
+            <NavLink to="/labTests" className="">
+              Lab Tests
+            </NavLink>
+            <NavLink to="/surgeries" className="">
+              Surgeries
+            </NavLink>
+            <NavLink to="/healthBlogs" className="">
+              Health Blogs
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
+
+// const Navbar = () => {
+// };
 
 export default Navbar;
