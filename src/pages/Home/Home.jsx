27c056d118ee1b2b0762a37379b8b73af0f95ea9
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-07-05 22:59:13 +0600

    added navbar from preline again

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,22 @@
-import AreYourADoctor from "../../components/Home/AreYourADoctor/AreYourADoctor";
-import Banner from "../../components/Home/Banner/Banner";
-import BestGynecologistDoctor from "../../components/Home/BestGynecologistDoctor/BestGynecologistDoctor";
-import BestSkinDoctor from "../../components/Home/BestSkinDoctor/BestSkinDoctor";
-import Blogs from "../../components/Home/Blogs/Blogs";
-import Diseases from "../../components/Home/Diseases/Diseases";
-import DonateYourBlood from "../../components/Home/DonateYourBlood/DonateYourBlood";
-import FreeMedicalAdvice from "../../components/Home/FreeMedicalAdvice/FreeMedicalAdvice";
-import Hospital from "../../components/Home/Hospital/Hospital";
-import OurPartners from "../../components/Home/OurPartners/OurPartners";
-import Reviews from "../../components/Home/Reviews/Reviews";
-import Symptoms from "../../components/Home/Symptoms/Symptoms";
-import VideoConsultation from "../../components/Home/VideoConsultation/VideoConsultation";
-import WhyChoose from "../../components/Home/WhyChoose/WhyChoose";
+// import AreYourADoctor from "../../components/Home/AreYourADoctor/AreYourADoctor";
+// import Banner from "../../components/Home/Banner/Banner";
+// import BestGynecologistDoctor from "../../components/Home/BestGynecologistDoctor/BestGynecologistDoctor";
+// import BestSkinDoctor from "../../components/Home/BestSkinDoctor/BestSkinDoctor";
+// import Blogs from "../../components/Home/Blogs/Blogs";
+// import Diseases from "../../components/Home/Diseases/Diseases";
+// import DonateYourBlood from "../../components/Home/DonateYourBlood/DonateYourBlood";
+// import FreeMedicalAdvice from "../../components/Home/FreeMedicalAdvice/FreeMedicalAdvice";
+// import Hospital from "../../components/Home/Hospital/Hospital";
+// import OurPartners from "../../components/Home/OurPartners/OurPartners";
+// import Reviews from "../../components/Home/Reviews/Reviews";
+// import Symptoms from "../../components/Home/Symptoms/Symptoms";
+// import VideoConsultation from "../../components/Home/VideoConsultation/VideoConsultation";
+// import WhyChoose from "../../components/Home/WhyChoose/WhyChoose";
 
 const Home = () => {
   return (
     <div>
-      <Banner></Banner>
+      {/* <Banner></Banner>
       <VideoConsultation></VideoConsultation>
       <FreeMedicalAdvice></FreeMedicalAdvice>
       <Symptoms></Symptoms>
@@ -29,7 +29,7 @@ const Home = () => {
       <Blogs></Blogs>
       <WhyChoose></WhyChoose>
       <OurPartners></OurPartners>
-      <AreYourADoctor></AreYourADoctor>
+      <AreYourADoctor></AreYourADoctor> */}
     </div>
   );
 };
