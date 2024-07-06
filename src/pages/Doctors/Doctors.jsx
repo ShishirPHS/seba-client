bf8ec554425383cbadcd5f73a373e71f7f2b0c4f
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-06 14:38:36 +0600

    fix navbar active class issue

--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -1,11 +1,7 @@
- 
-
 const Doctors = () => {
-    return (
-        <div>
-            this is doctors page.
-        </div>
-    );
+  return <div className="container mx-auto">
+    <h2 className="text-center py-[200px]">this is doctors page.</h2>
+  </div>;
 };
 
 export default Doctors;
