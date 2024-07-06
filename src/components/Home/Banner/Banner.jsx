61c7db24a374e2830df7b4524d1e5b5f1e13aec5
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-06 17:45:20 +0600

    trying to make banner responsive

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -16,15 +16,15 @@ const Banner = () => {
   };
 
   return (
-    <div className="bg-[#086060] banner relative">
-      <div className="w-[1589px] mx-auto flex">
+    <div className="bg-[#086060] banner relative px-[24px] md:px-0">
+      <div className="container mx-auto flex">
         {/* banner left */}
-        <div className="mb-[156px] bg-white rounded-[40px] pt-[49px] pb-[40px] px-16 mt-[177px] w-[50%]">
-          <h3 className="mb-[62px] font-poppins text-5xl font-bold max-w-[490px] leading-[123%]">
+        <div className="mb-[70px] md:mb-[156px] bg-white rounded-[40px] pt-[23px] md:pt-[49px] pb-[23px] md:pb-[40px] px-7 md:px-16 mt-[64px] md:mt-[177px] w-full md:w-[50%]">
+          <h3 className="mb-[25px] md:mb-[62px] font-poppins text-xl md:text-5xl text-center md:text-left font-bold max-w-[226px] md:max-w-[490px] leading-[123%]">
             Find the Best Doctor Near You
           </h3>
-          <form className="border border-[#808080] rounded-[40px]">
-            <div className="border-b border-[#808080] flex pt-[48px] px-[32px] pb-[20px]">
+          <form className="border border-[#808080] rounded-[25px] md:rounded-[40px] overflow-hidden">
+            <div className="border-b border-[#808080] flex pt-[28px] md:pt-[48px] px-[24px] md:px-[32px] pb-[14px] md:pb-[20px]">
               <input
                 type="text"
                 placeholder="Enter Your City or Location"
@@ -85,7 +85,7 @@ const Banner = () => {
         {/* banner right */}
         <div>
           <img
-            className="absolute bottom-0"
+            className="hidden md:block absolute bottom-0"
             src={bannerImg}
             alt="banner image"
           />
