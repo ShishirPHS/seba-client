9023e9a5c05bc1fe1b4c18392ba9e3a955d09f5e
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-07 19:49:02 +0600

    updated some margin padding and icon size

--- src/components/Home/Symptoms/Symptoms.jsx
+++ src/components/Home/Symptoms/Symptoms.jsx
@@ -7,9 +7,9 @@ import diarrhea from "../../../assets/icons/symptoms/viruses 1.svg";
 
 const Symptoms = () => {
   return (
-    <div className="container mx-auto mt-[38px] md:mt-[118px] mb-[140px] px-5">
+    <div className="container mx-auto mt-[38px] md:mt-[118px] mb-[60px] md:mb-[140px] px-5">
       {/* top */}
-      <div className="flex justify-between mb-[49px]">
+      <div className="flex justify-between mb-[35px] md:mb-[49px]">
         <p className="text-black font-poppins text-lg md:text-4xl font-medium leading-[123%]">
           Symptoms
         </p>
@@ -23,7 +23,7 @@ const Symptoms = () => {
         <div className="flex flex-col items-center">
           <img
             style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
-            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-10 md:w-auto h-10 md:h-auto"
+            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-20 md:w-auto h-20 md:h-auto"
             src={fever}
             alt="an icon of thermometer"
           />
@@ -35,7 +35,7 @@ const Symptoms = () => {
         <div className="flex flex-col items-center">
           <img
             style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
-            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-10 md:w-auto h-10 md:h-auto"
+            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-20 md:w-auto h-20 md:h-auto"
             src={heartAttack}
             alt="an icon of thermometer"
           />
@@ -47,7 +47,7 @@ const Symptoms = () => {
         <div className="flex flex-col items-center">
           <img
             style={{ boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)" }}
-            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-10 md:w-auto h-10 md:h-auto"
+            className="border border-[#086060] rounded-[18px] md:rounded-[22px] px-[24px] md:px-[45px] py-[22px] md:py-[40px] mb-[22px] w-20 md:w-auto h-20 md:h-auto"
             src={pregnancy}
             alt="an icon of thermometer"
           />
