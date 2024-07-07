390e2e3f448505673084171fd79c651fd6ef3f2d
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-07-07 10:47:35 +0600

    improved responsiveness of freeMedicalAdvice part

--- src/components/Home/FreeMedicalAdvice/FreeMedicalAdvice.jsx
+++ src/components/Home/FreeMedicalAdvice/FreeMedicalAdvice.jsx
@@ -4,23 +4,27 @@ import img3 from "../../../assets/video-consultation/consultation-3.png";
 
 const FreeMedicalAdvice = () => {
   return (
-    <div className="container mx-auto">
-      <div className="flex gap-[78px]">
+    <div className="container mx-auto px-5">
+      <div className="flex flex-col md:flex-row gap-[28px] md:gap-[78px]">
         {/* left */}
-        <div className="flex flex-col justify-center items-center pt-[72px] px-[118px] bg-[#FF0068] rounded-[40px] flex-shrink-0">
-          <h3 className="text-4xl font-bold text-white font-poppins leading-[123%]">
+        <div className="flex flex-col justify-center items-center pt-[47px] md:pt-[72px] px-[80px] md:px-[118px] bg-[#FF0068] rounded-[24px] md:rounded-[40px] flex-shrink-0">
+          <h3 className="text-[19px] md:text-4xl font-bold text-white font-poppins leading-[123%]">
             Video Consultation
           </h3>
-          <p className="font-poppins text-2xl font-normal leading-[123%] text-white">
+          <p className="font-poppins text-[16px] md:text-2xl font-normal leading-[123%] text-white">
             Verified Doctors
           </p>
-          <img className="mt-[33px]" src={img1} alt="" />
+          <img
+            className="w-[210px] md:w-auto h-[245px] md:h-auto mt-[21px] md:mt-[33px]"
+            src={img1}
+            alt=""
+          />
         </div>
         {/* right */}
-        <div className="flex flex-col gap-[51px] w-full">
+        <div className="flex flex-col gap-[23px] md:gap-[51px] w-full">
           {/* right top */}
-          <div className="bg-[#3BA4A7] rounded-[40px] pl-[70px] pr-[76px] pt-[54px] pb-[58px] relative">
-            <h3 className="text-white font-poppins text-2xl font-bold leading-[123%] mb-[21px] max-w-[418px]">
+          <div className="bg-[#3BA4A7] rounded-[40px] pl-[32px] md:pl-[70px] pr-[76px] pt-[24px] md:pt-[54px] pb-[23px] md:pb-[58px] relative">
+            <h3 className="text-white font-poppins text-[9px] md:text-2xl font-bold leading-[123%] mb-[10px] md:mb-[21px] max-w-[418px]">
               Get free medical advice by asking a doctor
             </h3>
             {/* btn div */}
@@ -35,15 +39,15 @@ const FreeMedicalAdvice = () => {
             <img className="absolute bottom-0 right-[76px]" src={img2} alt="" />
           </div>
           {/* right bottom */}
-          <div className="bg-[#1F1F1F] rounded-[40px] pt-[53px] pb-[50px] pl-[70px] relative">
-            <h3 className="max-w-[259px] text-white font-poppins text-2xl font-bold leading-[123%]">
+          <div className="bg-[#1F1F1F] rounded-[40px] pt-[23px] md:pt-[53px] pb-[23px] md:pb-[50px] pl-[32px] md:pl-[70px] relative">
+            <h3 className="max-w-[259px] text-white font-poppins text-[9px] md:text-2xl font-bold leading-[123%]">
               Get 10% OFF ON YOUR FIRST CALL
             </h3>
             <button
               style={{
                 boxShadow: "0px 4px 50px 0px rgba(255, 255, 255, 0.21)",
               }}
-              className="py-[19px] px-[47px] rounded-[22px] bg-white mt-[30px] font-poppins text-[15px] font-medium leading-[123%] text-black"
+              className="py-[19px] px-[47px] rounded-[22px] bg-white mt-[10px] md:mt-[30px] font-poppins text-[15px] font-medium leading-[123%] text-black"
             >
               Find Doctor
             </button>
--- src/components/Home/VideoConsultation/VideoConsultation.jsx
+++ src/components/Home/VideoConsultation/VideoConsultation.jsx
@@ -8,7 +8,7 @@ import videoConsultation from "../../../assets/icons/video-consultation/video-co
 
 const VideoConsultation = () => {
   return (
-    <div className="bg-[#3BA4A7] py-1 md:pt-[17px] md:pb-[20px] mb-[14px] md:mb-[123px]">
+    <div className="bg-[#3BA4A7] py-1 md:pt-[17px] md:pb-[20px] mb-[18px] md:mb-[123px]">
       <div className="container mx-auto">
         <div className="flex justify-between relative">
           <div className="bg-[#086060] pt-[5px] md:pt-[17px] pb-[7px] md:pb-[20px] px-[8px] md:px-[40px] rounded-[5px] md:rounded-[20px]">
