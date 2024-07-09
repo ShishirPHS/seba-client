df3d27f2c72f83104f615920dd53303334f73d34
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-09 18:39:26 +0600

    created whyChooseSeba section for desktop devices

--- src/components/Home/WhyChoose/WhyChoose.jsx
+++ src/components/Home/WhyChoose/WhyChoose.jsx
@@ -8,8 +8,40 @@ const WhyChoose = () => {
         Why Choose Seba?
       </h3>
       {/* img div */}
-      <div className="mb-[83px] mt-[60px] relative flex justify-center items-center">
-        <img src={mobileImg} alt="" />
+      <div className="mb-[83px] mt-[60px] relative">
+        <img className="block mx-auto" src={mobileImg} alt="" />
+        <p
+          style={{
+            boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
+          }}
+          className="absolute right-[57%] top-[57px] rounded-[17px] py-[25px] pl-[25px] pr-[54px] text-black text-2xl font-medium font-poppins leading-[123%] bg-[#EAECFF]"
+        >
+          BMDC verified doctor
+        </p>
+        <p
+          style={{
+            boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
+          }}
+          className="absolute right-[56.5%] top-[297px] rounded-[17px] py-[25px] pl-[25px] pr-[54px] text-black text-2xl font-medium font-poppins leading-[123%] bg-[#FFF1F2]"
+        >
+          No more Prescription lost
+        </p>
+        <p
+          style={{
+            boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
+          }}
+          className="absolute left-[54.5%] top-[113px] rounded-[17px] py-[25px] pl-[25px] pr-[54px] text-black text-2xl font-medium font-poppins leading-[123%] bg-[#FFF5E4]"
+        >
+          Blood donor finding
+        </p>
+        <p
+          style={{
+            boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
+          }}
+          className="absolute left-[57%] top-[487px] rounded-[17px] py-[25px] pl-[25px] pr-[54px] text-black text-2xl font-medium font-poppins leading-[123%] bg-[#E3FFE5]"
+        >
+          24/7 online doctor
+        </p>
       </div>
       {/* bottom div */}
       <div className="flex justify-between items-center gap-6">
