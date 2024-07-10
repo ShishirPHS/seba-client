17399ee99329523bcf57bdea67d0d90a092a8e9b
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-10 17:30:28 +0600

    added container class to the bestSkinDcotor and bestGynecologistDoctor part

--- src/components/Home/BestSkinDoctor/BestSkinDoctor.jsx
+++ src/components/Home/BestSkinDoctor/BestSkinDoctor.jsx
@@ -15,36 +15,38 @@ const BestSkinDoctor = () => {
 
   return (
     <div
-      className="w-[1589px] mx-auto mt-[128px] mb-[60px]"
+      className="container mx-auto mt-[128px] mb-[60px] "
       id="bestSkinDoctor"
     >
-      <h3 className="text-black font-poppins text-4xl font-medium leading-[123%] capitalize mb-[26px]">
-        best skin doctor of the month
-      </h3>
-      {bestSkinDoctors.length && (
-        <div>
-          <Swiper
-            slidesPerView={3}
-            spaceBetween={30}
-            loop={true}
-            autoplay={{
-              delay: 2000,
-              disableOnInteraction: false,
-            }}
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Autoplay]}
-            className="mySwiper"
-          >
-            {bestSkinDoctors.map((doctor) => (
-              <SwiperSlide key={doctor.id}>
-                <SingleRatingCard doctor={doctor}></SingleRatingCard>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      )}
+      <div className="mx-5">
+        <h3 className="text-black font-poppins text-4xl font-medium leading-[123%] capitalize mb-[26px]">
+          best skin doctor of the month
+        </h3>
+        {bestSkinDoctors.length && (
+          <div>
+            <Swiper
+              slidesPerView={3}
+              spaceBetween={30}
+              loop={true}
+              autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay]}
+              className="mySwiper"
+            >
+              {bestSkinDoctors.map((doctor) => (
+                <SwiperSlide key={doctor.id}>
+                  <SingleRatingCard doctor={doctor}></SingleRatingCard>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
