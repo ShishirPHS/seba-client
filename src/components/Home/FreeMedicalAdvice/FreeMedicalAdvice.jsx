4a51ca0a44e3961d4b65649c86a315c7e1379630
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-07-13 18:29:28 +0600

    added z-index to some text

--- src/components/Home/FreeMedicalAdvice/FreeMedicalAdvice.jsx
+++ src/components/Home/FreeMedicalAdvice/FreeMedicalAdvice.jsx
@@ -8,10 +8,10 @@ const FreeMedicalAdvice = () => {
       <div className="flex flex-col md:flex-row gap-[28px] md:gap-[78px]">
         {/* left */}
         <div className="flex flex-col pt-[47px] md:pt-[72px] px-[80px] md:px-[118px] bg-[#FF0068] rounded-[24px] md:rounded-[40px] flex-shrink-0 relative overflow-hidden">
-          <h3 className="text-[19px] md:text-4xl font-bold text-white font-poppins leading-[123%] inline-block mx-auto">
+          <h3 className="text-[19px] md:text-4xl font-bold text-white font-poppins leading-[123%] inline-block mx-auto relative z-50">
             Video Consultation
           </h3>
-          <p className="font-poppins text-[16px] md:text-2xl font-normal leading-[123%] text-white inline-block mx-auto mb-[410px] md:mb-0">
+          <p className="font-poppins text-[16px] md:text-2xl font-normal leading-[123%] text-white inline-block mx-auto mb-[410px] md:mb-0 relative z-50">
             Verified Doctors
           </p>
           <div className="w-[319px] md:w-[341px] h-[372px] md:h-[372px] mt-[21px] md:mt-[33px] absolute left-[50%] transform -translate-x-[50%] bottom-0">
