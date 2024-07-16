3d22a1d83adcb87b1e016450eef9daf01d9ec242
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-16 16:45:13 +0600

    made blogs part responsive for lg device

--- src/components/Home/Blogs/Blogs.jsx
+++ src/components/Home/Blogs/Blogs.jsx
@@ -20,16 +20,23 @@ const Blogs = () => {
           sectionHeader={"Health Blogs"}
           sectionSubHeader={"Find out how our users are spreading the world"}
         ></SectionHeader>
-        <div>
+        <div className="mt-[64px] lg:mt-20 2xl:mt-[120px]">
           {/* for small devices */}
-          <div className="md:hidden mt-[64px] md:mt-[120px] grid grid-cols-2 lg:grid-cols-3 gap-[25px] md:gap-[39px]">
+          <div className="md:hidden grid grid-cols-2 lg:grid-cols-3 gap-[25px] md:gap-[39px]">
             {smallDeviceBlogs.map((blog) => (
               <SingleBlogCard key={blog.id} blog={blog}></SingleBlogCard>
             ))}
           </div>
 
-          {/* for large devices */}
-          <div className="hidden mt-[64px] md:mt-[120px] md:grid grid-cols-2 lg:grid-cols-3 gap-[25px] md:gap-[39px]">
+          {/* for lg devices */}
+          <div className="hidden lg:grid xl:hidden grid-cols-2 gap-[25px] md:gap-[39px]">
+            {smallDeviceBlogs.map((blog) => (
+              <SingleBlogCard key={blog.id} blog={blog}></SingleBlogCard>
+            ))}
+          </div>
+
+          {/* for 2xl devices */}
+          <div className="hidden xl:grid grid-cols-2 lg:grid-cols-3 gap-[25px] md:gap-[39px]">
             {largeDeviceBlogs.map((blog) => (
               <SingleBlogCard key={blog.id} blog={blog}></SingleBlogCard>
             ))}
@@ -42,9 +49,9 @@ const Blogs = () => {
             style={{
               filter: "blur(50px)",
             }}
-            className="h-[91px] md:h-[288px] w-full bg-gradient-to-b from-[#013131] to-[#013131b3] -mt-[33px] md:-mt-[124px] z-20 relative"
+            className="h-[91px] lg:h-[198px] xl:h-[288px] w-full bg-gradient-to-b from-[#013131] to-[#013131b3] -mt-[33px] lg:-mt-[67px] xl:-mt-[124px] z-20 relative"
           ></div>
-          <button className="text-white font-poppins text-[10px] md:text-[32px] font-semibold leading-[123%] bg-[#d9d9d900] rounded-[12px] md:rounded-[40px] px-[25px] md:px-[69px] py-[12px] md:py-[36px] border-white border-[2px] absolute bottom-0 left-[50%] transform -translate-x-[50%] z-30">
+          <button className="text-white font-poppins text-[10px] lg:text-xl xl:text-[32px] font-semibold leading-[123%] bg-[#d9d9d900] rounded-[12px] lg:rounded-[20px] xl:rounded-[40px] px-[25px] lg:px-[50px] xl:px-[69px] py-[12px] lg:py-5 xl:py-[36px] border-white border-[2px] absolute bottom-0 left-[50%] transform -translate-x-[50%] z-30">
             See more
           </button>
         </div>
--- src/components/Home/Blogs/SingleBlogCard/SingleBlogCard.jsx
+++ src/components/Home/Blogs/SingleBlogCard/SingleBlogCard.jsx
@@ -4,13 +4,13 @@ const SingleBlogCard = ({ blog }) => {
   const { image, blogName } = blog;
 
   return (
-    <div className="bg-[#747474a1] rounded-[20px] md:rounded-[40px] p-[10px] md:p-5 pr-[70px] flex space-x-[16px] md:space-x-[58px] items-center">
+    <div className="bg-[#747474a1] rounded-[20px] lg:rounded-[30px] xl:rounded-[40px] p-[10px] lg:p-[25px] xl:p-5 pr-[70px] flex space-x-[16px] lg:space-x-[100px] xl:space-x-[58px] items-center">
       <img
-        className="rounded-[20px] md:rounded-[40px] w-[62px] md:w-auto h-[54px] md:h-auto"
+        className="rounded-[20px] xl:rounded-[40px] w-[62px] lg:w-[150px] xl:w-auto h-[54px] md:h-auto"
         src={image}
         alt=""
       />
-      <p className="font-poppins text-white text-[12px] md:text-[32px] font-semibold leading-[123%] max-w-[140px]">
+      <p className="font-poppins text-white text-[12px] lg:text-[22px] xl:text-[32px] font-semibold leading-[123%] max-w-[140px] lg:max-w-[100px] xl:max-w-[140px]">
         {blogName}
       </p>
     </div>
