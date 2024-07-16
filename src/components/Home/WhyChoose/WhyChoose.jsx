c0297d4c61da33ba9bb4a06d9658cfaf8b3b4208
shishir-ahmed <shishirahmed790@gmail.com>
2024-07-16 18:30:03 +0600

    trying to make whyChoose part responsive for lg device

--- src/components/Home/WhyChoose/WhyChoose.jsx
+++ src/components/Home/WhyChoose/WhyChoose.jsx
@@ -2,15 +2,15 @@ import mobileImg from "../../../assets/why-choose/mobileImage.png";
 
 const WhyChoose = () => {
   return (
-    <div className="container mx-auto mb-[46px] md:mb-[142px] mt-[56px] md:mt-[114px] px-5">
+    <div className="container mx-auto mb-[46px] lg:mb-[90px] xl:mb-[142px] mt-[56px] lg:mt-[90px] xl:mt-[114px] px-5">
       {/* title */}
-      <h3 className="text-[#232222] text-[16px] md:text-5xl font-semibold font-poppins leading-[123%] mx-auto text-center">
+      <h3 className="text-[#232222] text-[16px] lg:text-[34px] xl:text-5xl font-semibold font-poppins leading-[123%] mx-auto text-center">
         Why Choose Seba?
       </h3>
       {/* img div */}
-      <div className="mb-[23px] md:mb-[83px] mt-[23px] md:mt-[60px] relative">
+      <div className="mb-[23px] lg:mb-[63px] xl:mb-[83px] mt-[23px] md:mt-[60px] relative">
         <img
-          className="block mx-auto h-[255px] md:h-auto"
+          className="block mx-auto h-[255px] lg:h-[600px] xl:h-auto"
           src={mobileImg}
           alt=""
         />
@@ -18,7 +18,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute right-[62%] md:right-[57%] top-[22px] md:top-[57px] rounded-[17px] py-[10px] md:py-[25px] pl-[23px] md:pl-[25px] pr-[24px] md:pr-[54px] text-black text-[7px] md:text-2xl font-medium font-poppins leading-[123%] bg-[#EAECFF]"
+          className="absolute right-[62%] lg:right-[62.5%] xl:right-[57%] top-[22px] lg:top-[67px] xl:top-[57px] rounded-[17px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#EAECFF]"
         >
           BMDC verified doctor
         </p>
@@ -26,7 +26,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute right-[61%] md:right-[56.5%] top-[117px] md:top-[297px] rounded-[17px] py-[10px] md:py-[25px] pl-[23px] md:pl-[25px] pr-[24px] md:pr-[54px] text-black text-[7px] md:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF1F2]"
+          className="absolute right-[61%] lg:right-[62.5%] xl:right-[56.5%] top-[117px] lg:top-[301px] xl:top-[297px] rounded-[17px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF1F2]"
         >
           No more Prescription lost
         </p>
@@ -34,7 +34,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute left-[59%] md:left-[54.5%] top-[44px] md:top-[113px] rounded-[17px] py-[10px] md:py-[25px] pl-[23px] md:pl-[25px] pr-[24px] md:pr-[54px] text-black text-[7px] md:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF5E4]"
+          className="absolute left-[59%] lg:left-[58.5%] xl:left-[54.5%] top-[44px] lg:top-[105px] xl:top-[113px] rounded-[17px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF5E4]"
         >
           Blood donor finding
         </p>
@@ -42,7 +42,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute left-[62.5%] md:left-[57%] top-[193px] md:top-[487px] rounded-[17px] py-[10px] md:py-[25px] pl-[23px] md:pl-[25px] pr-[24px] md:pr-[54px] text-black text-[7px] md:text-2xl font-medium font-poppins leading-[123%] bg-[#E3FFE5]"
+          className="absolute left-[62.5%] lg:left-[62.5%] xl:left-[57%] top-[193px] lg:top-[457px] xl:top-[487px] rounded-[17px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#E3FFE5]"
         >
           24/7 online doctor
         </p>
