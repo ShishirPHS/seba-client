9458f80db7a0fe816ddc01a8f5873f376ef255b9
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-07-17 18:24:05 +0600

    made navbar responsive for xl device

--- src/layout/Root/Root.jsx
+++ src/layout/Root/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../../components/shared/Footer/Footer";
+// import Footer from "../../components/shared/Footer/Footer";
 import Navbar from "../../components/shared/Navbar/Navbar";
 
 const Root = () => {
@@ -7,7 +7,7 @@ const Root = () => {
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </div>
   );
 };
