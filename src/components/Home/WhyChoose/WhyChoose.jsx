7ba9398fb9808b59c9b1b92c16b0a34fb09f2dcd
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-07-27 13:55:50 +0600

    made whyChooseSeba part responsive for xl device

--- src/components/Home/WhyChoose/WhyChoose.jsx
+++ src/components/Home/WhyChoose/WhyChoose.jsx
@@ -2,9 +2,9 @@ import mobileImg from "../../../assets/why-choose/mobileImage.png";
 
 const WhyChoose = () => {
   return (
-    <div className="container mx-auto mb-[46px] lg:mb-[90px] xl:mb-[142px] mt-[56px] lg:mt-[90px] xl:mt-[114px] px-5">
+    <div className="container mx-auto mb-[46px] lg:mb-[90px] xl:mb-[115px] 2xl:mb-[142px] mt-[56px] lg:mt-[90px] xl:mt-[100px] 2xl:mt-[114px] px-5">
       {/* title */}
-      <h3 className="text-[#232222] text-[16px] lg:text-[34px] xl:text-5xl font-semibold font-poppins leading-[123%] mx-auto text-center">
+      <h3 className="text-[#232222] text-[16px] lg:text-[34px] xl:text-[44px] 2xl:text-5xl font-semibold font-poppins leading-[123%] mx-auto text-center">
         Why Choose Seba?
       </h3>
       {/* img div */}
@@ -18,7 +18,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute right-[62%] lg:right-[62.5%] xl:right-[57%] top-[22px] lg:top-[67px] xl:top-[57px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#EAECFF]"
+          className="absolute right-[62%] lg:right-[62.5%] xl:right-[61%] 2xl:right-[57%] top-[22px] lg:top-[67px] xl:top-[57px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#EAECFF]"
         >
           BMDC verified doctor
         </p>
@@ -26,7 +26,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute right-[61%] lg:right-[62.5%] xl:right-[56.5%] top-[117px] lg:top-[301px] xl:top-[297px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF1F2]"
+          className="absolute right-[61%] lg:right-[62.5%] xl:right-[61%] 2xl:right-[56.5%] top-[117px] lg:top-[301px] xl:top-[297px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF1F2]"
         >
           No more Prescription lost
         </p>
@@ -34,7 +34,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute left-[59%] lg:left-[58.5%] xl:left-[54.5%] top-[44px] lg:top-[105px] xl:top-[113px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF5E4]"
+          className="absolute left-[59%] lg:left-[58.5%] xl:left-[57%] 2xl:left-[54.5%] top-[44px] lg:top-[105px] xl:top-[113px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#FFF5E4]"
         >
           Blood donor finding
         </p>
@@ -42,7 +42,7 @@ const WhyChoose = () => {
           style={{
             boxShadow: "0px 22px 60px 0px rgba(0, 0, 0, 0.10)",
           }}
-          className="absolute left-[62.5%] lg:left-[62.5%] xl:left-[57%] top-[193px] lg:top-[457px] xl:top-[487px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#E3FFE5]"
+          className="absolute left-[62.5%] lg:left-[62.5%] xl:left-[61%] 2xl:left-[57%] top-[193px] lg:top-[457px] xl:top-[487px] rounded-[10px] lg:rounded-[12px] xl:rounded-[17px] py-[10px] lg:py-[12px] xl:py-[25px] pl-[23px] lg:pl-[20px] xl:pl-[25px] pr-[24px] lg:pr-[40px] xl:pr-[54px] text-black text-[7px] lg:text-base xl:text-2xl font-medium font-poppins leading-[123%] bg-[#E3FFE5]"
         >
           24/7 online doctor
         </p>
@@ -52,19 +52,19 @@ const WhyChoose = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* bottom left */}
         <div className="col-span-1 rounded-[17px] px-[43px] lg:px-5 xl:px-[30px] py-[20px] lg:py-4 xl:py-[18px] bg-[#086060] w-full md:w-auto">
-          <p className="font-poppins text-base xl:text-2xl font-medium leading-[123%] text-white text-center md:text-left max-w-[300px] md:max-w-none mx-auto">
+          <p className="font-poppins text-base xl:text-lg 2xl:text-2xl font-medium leading-[123%] text-white text-center md:text-left max-w-[300px] md:max-w-none mx-auto">
             Country First Doctor Patients Relation Management System
           </p>
         </div>
         {/* bottom middle */}
-        <div className="col-span-1 rounded-[17px] pl-[72px] lg:pl-5 xl:pl-[72px] pr-[64px] lg:pr-[20px] xl:pr-[64px] py-[20px] lg:py-4 xl:py-[18px] bg-[#F4F4F4] w-full md:w-auto">
-          <p className="font-poppins text-base xl:text-2xl font-medium leading-[123%] text-black text-center md:text-left max-w-[243px] md:max-w-none mx-auto">
+        <div className="col-span-1 rounded-[17px] pl-[72px] lg:pl-5 xl:pl-[62px] 2xl:pl-[72px] pr-[64px] lg:pr-[20px] xl:pr-[52px] 2xl:pr-[64px] py-[20px] lg:py-4 xl:py-[18px] bg-[#F4F4F4] w-full md:w-auto">
+          <p className="font-poppins text-base xl:text-lg 2xl:text-2xl font-medium leading-[123%] text-black text-center md:text-left max-w-[243px] md:max-w-none mx-auto">
             Online booking facility to save time during physical visit
           </p>
         </div>
         {/* bottom right */}
         <div className="col-span-1 rounded-[17px] px-[80px] lg:px-[20px] xl:px-[60px] py-[18px] lg:py-4 xl:py-[18px] bg-[#FF0068] w-full md:w-auto">
-          <p className="font-poppins text-base xl:text-2xl font-medium leading-[123%] text-white text-center md:text-left max-w-[220px] md:max-w-none mx-auto">
+          <p className="font-poppins text-base xl:text-lg 2xl:text-2xl font-medium leading-[123%] text-white text-center md:text-left max-w-[220px] md:max-w-none mx-auto">
             Timely medicine taking reminder using mobile app
           </p>
         </div>
