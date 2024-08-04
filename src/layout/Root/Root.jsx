a3e420d7b334ebfd19af9b5aa5f0ecf1fd7b68cd
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-08-04 18:06:17 +0600

    added preline reinitializer

--- src/layout/Root/Root.jsx
+++ src/layout/Root/Root.jsx
@@ -2,7 +2,14 @@ import { Outlet } from "react-router-dom";
 import Footer from "../../components/shared/Footer/Footer";
 import Navbar from "../../components/shared/Navbar/Navbar";
 
+import "preline/preline";
+import { useEffect } from "react";
+
 const Root = () => {
+  useEffect(() => {
+    window.HSStaticMethods.autoInit();
+  }, []);
+
   return (
     <div>
       <Navbar></Navbar>
