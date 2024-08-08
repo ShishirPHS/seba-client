df4e316f7fbbc3cf7e63585adc95f938c94283c5
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-08 18:27:00 +0600

    made symptoms part responsive for all devices

--- src/components/Home/Symptoms/SingleSymptomsCard/SingleSymptomsCard.jsx
+++ src/components/Home/Symptoms/SingleSymptomsCard/SingleSymptomsCard.jsx
@@ -11,12 +11,12 @@ const SingleSymptomsCard = ({ symptom }) => {
       >
         <img
           style={{ boxSizing: "content-box" }}
-          className="p-4 rounded-[18px] md:rounded-[22px] px-[24px] lg:px-[35px] xl:px-[45px] py-[22px] lg:py-[30px] xl:py-[40px] w-10 lg:w-[50px] xl:w-auto h-10 md:h-auto"
+          className="p-4 rounded-[18px] md:rounded-[22px] px-[20px] sm:px-[24px] lg:px-[35px] xl:px-[45px] py-[18px] sm:py-[22px] lg:py-[30px] xl:py-[40px] w-[34px] sm:w-10 lg:w-[50px] xl:w-auto h-[34px] sm:h-10 md:h-auto"
           src={image}
           alt={`An icon representing ${symptomsName}`}
         />
       </div>
-      <p className="text-black font-poppins text-xs lg:text-lg xl:text-2xl font-medium leading-[123%]">
+      <p className="text-black font-poppins text-xs sm:text-sm md:text-base lg:text-lg xl:text-2xl font-medium leading-[123%]">
         {symptomsName}
       </p>
     </div>
--- src/components/Home/Symptoms/Symptoms.jsx
+++ src/components/Home/Symptoms/Symptoms.jsx
@@ -4,8 +4,8 @@ import SingleSymptomsCard from "./SingleSymptomsCard/SingleSymptomsCard";
 const Symptoms = () => {
   const [symptomsInfo] = useSymptoms();
 
-  const smallDeviceSymptoms = symptomsInfo.slice(0, 3);
-  const lgDeviceSymptoms = symptomsInfo.slice(0, 4);
+  const extraSmallDeviceSymptoms = symptomsInfo.slice(0, 3);
+  const smDeviceSymptoms = symptomsInfo.slice(0, 4);
   const xlDeviceSymptoms = symptomsInfo.slice(0, 5);
   const xxlDeviceSymptoms = symptomsInfo.slice(0, 6);
 
@@ -13,32 +13,32 @@ const Symptoms = () => {
     <div className="container mx-auto mt-[38px] lg:mt-[95px] xl:mt-[105px] 2xl:mt-[118px] mb-[60px] lg:mb-[100px] xl:mb-[115px] 2xl:mb-[140px] px-5">
       {/* top */}
       <div className="flex justify-between mb-[35px] lg:mb-10 xl:mb-[49px]">
-        <p className="text-black font-poppins text-lg lg:text-2xl xl:text-[34px] 2xl:text-4xl font-medium leading-[123%]">
+        <p className="text-black font-poppins text-lg sm:text-[22px] md:text-2xl lg:text-2xl xl:text-[34px] 2xl:text-4xl font-medium leading-[123%]">
           Symptoms
         </p>
-        <p className="text-black font-poppins text-lg lg:text-2xl xl:text-[34px] 2xl:text-4xl font-medium leading-[123%] cursor-pointer">
+        <p className="text-black font-poppins text-lg sm:text-[22px] md:text-2xl lg:text-2xl xl:text-[34px] 2xl:text-4xl font-medium leading-[123%] cursor-pointer">
           View All
         </p>
       </div>
       {/* bottom */}
       <div>
-        <div className="flex lg:hidden flex-wrap gap-6 justify-between">
-          {smallDeviceSymptoms.map((singleSymptoms) => (
+        <div className="flex sm:hidden flex-wrap gap-4 lg:gap-6 justify-between">
+          {extraSmallDeviceSymptoms.map((singleSymptoms) => (
             <SingleSymptomsCard
               key={singleSymptoms.id}
               symptom={singleSymptoms}
             ></SingleSymptomsCard>
           ))}
         </div>
-        <div className="hidden lg:flex xl:hidden flex-wrap gap-6 justify-between">
-          {lgDeviceSymptoms.map((singleSymptoms) => (
+        <div className="hidden sm:flex xl:hidden flex-wrap gap-4 lg:gap-6 justify-between">
+          {smDeviceSymptoms.map((singleSymptoms) => (
             <SingleSymptomsCard
               key={singleSymptoms.id}
               symptom={singleSymptoms}
             ></SingleSymptomsCard>
           ))}
         </div>
-        <div className="hidden xl:flex 2xl:hidden flex-wrap gap-6 justify-between">
+        <div className="hidden xl:flex 2xl:hidden flex-wrap gap-4 lg:gap-6 justify-between">
           {xlDeviceSymptoms.map((singleSymptoms) => (
             <SingleSymptomsCard
               key={singleSymptoms.id}
@@ -46,7 +46,7 @@ const Symptoms = () => {
             ></SingleSymptomsCard>
           ))}
         </div>
-        <div className="hidden 2xl:flex flex-wrap gap-6 justify-between">
+        <div className="hidden 2xl:flex flex-wrap gap-4 lg:gap-6 justify-between">
           {xxlDeviceSymptoms.map((singleSymptoms) => (
             <SingleSymptomsCard
               key={singleSymptoms.id}
