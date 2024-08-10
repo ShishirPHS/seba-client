e12128590a682c839dddd68f732f6d29883f933e
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-10 14:08:27 +0600

    made reviews part responsive for sm devices

--- src/components/Home/Reviews/SingleReviewCard/SingleReviewCard.jsx
+++ src/components/Home/Reviews/SingleReviewCard/SingleReviewCard.jsx
@@ -12,16 +12,16 @@ const SingleReviewCard = ({ review }) => {
   return (
     <div
       style={{ backgroundColor: `${bgColor}` }}
-      className="bg-[#E3FFE5] pt-[12px] lg:pt-[20px] xl:pt-[26px] 2xl:pt-[31px] pb-[30px] lg:pb-[45px] xl:pb-[60px] 2xl:pb-[90px] pl-[12px] lg:pl-[20px] xl:pl-[24px] 2xl:pl-[27px] pr-[16px] xl:pr-[28px] 2xl:pr-[34px] rounded-[16px] lg:rounded-[25px] xl:rounded-[36px] 2xl:rounded-[40px] relative"
+      className="bg-[#E3FFE5] pt-[12px] lg:pt-[20px] xl:pt-[26px] 2xl:pt-[31px] pb-[30px] sm:pb-[38px] lg:pb-[45px] xl:pb-[60px] 2xl:pb-[90px] pl-[12px] lg:pl-[20px] xl:pl-[24px] 2xl:pl-[27px] pr-[16px] xl:pr-[28px] 2xl:pr-[34px] rounded-[16px] lg:rounded-[25px] xl:rounded-[36px] 2xl:rounded-[40px] relative"
     >
       {/* profile */}
-      <div className="flex items-center gap-[6px] md:gap-[12px]">
-        <img className="rounded-full w-10 lg:w-12 xl:w-auto" src={img} alt="" />
+      <div className="flex items-center gap-[6px] sm:gap-3 md:gap-[12px]">
+        <img className="rounded-full w-10 sm:w-[46px] lg:w-12 xl:w-auto" src={img} alt="" />
         <div className="space-y-[3px] md:space-y-0">
-          <h6 className="text-black font-poppins text-[8px] lg:text-[15px] xl:text-xl font-semibold leading-[123%]">
+          <h6 className="text-black font-poppins text-[8px] sm:text-sm lg:text-[15px] xl:text-xl font-semibold leading-[123%]">
             {name}
           </h6>
-          <p className="text-black font-poppins text-[5px] lg:text-xs xl:text-sm font-light leading-[123%]">
+          <p className="text-black font-poppins text-[5px] sm:text-[9px] lg:text-xs xl:text-sm font-light leading-[123%]">
             {userName}
           </p>
         </div>
@@ -29,7 +29,7 @@ const SingleReviewCard = ({ review }) => {
       {/* comment */}
       <div className="mt-[8px] lg:mt-3 xl:mt-[18px]">
         {/* for small device */}
-        <p className="block md:hidden text-[#514F6E] text-[8px] md:text-lg font-dmSans leading-[166%] max-w-[158px] md:max-w-[432px]">
+        <p className="block md:hidden text-[#514F6E] text-[8px] sm:text-sm font-dmSans leading-[166%] max-w-[158px] sm:max-w-[260px]">
           {oneSentence}
         </p>
         {/* comment (greater than sm devices) */}
@@ -37,7 +37,7 @@ const SingleReviewCard = ({ review }) => {
           {comment}
         </p>
       </div>
-      <p className="font-dmSans text-black text-[5px] lg:text-base xl:text-lg font-bold leading-[166%] absolute left-[14px] md:left-auto md:right-[33px] md:top-[21px] bottom-[10px] md:bottom-auto">
+      <p className="font-dmSans text-black text-[5px] sm:text-[9px] lg:text-base xl:text-lg font-bold leading-[166%] absolute left-[14px] md:left-auto md:right-[33px] md:top-[21px] bottom-[10px] md:bottom-auto">
         {rating}
       </p>
     </div>
