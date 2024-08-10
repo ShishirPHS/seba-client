d4a3f522277e830d24f63c071118df6400cdd5d6
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-10 19:00:32 +0600

    made areYouADoctor part responsive for all devices

--- src/components/Home/AreYourADoctor/AreYourADoctor.jsx
+++ src/components/Home/AreYourADoctor/AreYourADoctor.jsx
@@ -3,7 +3,7 @@ import doctors from "../../../assets/are-you-a-doctor/doctors.png";
 const AreYourADoctor = () => {
   return (
     <div className="px-5">
-      <div className="bg-[#E3FFE5] container mx-auto rounded-[40px] lg:rounded-[24px] xl:rounded-[40px] pt-[21px] lg:pt-10 xl:pt-[70px] 2xl:pt-[79px] pb-0 lg:pb-10 xl:pb-[80px] 2xl:pb-[88px] pr-0 lg:pr-[60px] xl:pr-[90px] 2xl:pr-[186px] flex flex-col md:flex-row items-center md:justify-end relative mb-[66px] lg:mb-[110px] xl:mb-[135px] 2xl:mb-[160px]">
+      <div className="bg-[#E3FFE5] container mx-auto rounded-[40px] md:rounded-[30px] lg:rounded-[24px] xl:rounded-[40px] pt-[21px] md:mt-[30px] lg:pt-10 xl:pt-[70px] 2xl:pt-[79px] pb-0 md:pb-10 xl:pb-[80px] 2xl:pb-[88px] pr-0 md:pr-[50px] lg:pr-[60px] xl:pr-[90px] 2xl:pr-[186px] flex flex-col md:flex-row items-center md:justify-end relative mb-[66px] lg:mb-[110px] xl:mb-[135px] 2xl:mb-[160px]">
         <div>
           <h4 className="font-poppins text-[20px] lg:text-[28px] xl:text-[30px] 2xl:text-4xl text-center md:text-left font-bold text-[#086060] mb-0 xl:mb-[8px] 2xl:mb-[10px]">
             Are You A Doctor?
@@ -16,7 +16,7 @@ const AreYourADoctor = () => {
           </button>
         </div>
         <img
-          className="block md:absolute bottom-0 left-[109px] lg:left-[70px] xl:left-[90px] 2xl:left-[109px] w-[201px] lg:w-[360px] xl:w-[520px] 2xl:w-auto"
+          className="block md:absolute bottom-0 left-[109px] md:left-[70px] lg:left-[70px] xl:left-[90px] 2xl:left-[109px] w-[201px] md:w-[222px] lg:w-[360px] xl:w-[520px] 2xl:w-auto"
           src={doctors}
           alt=""
         />
