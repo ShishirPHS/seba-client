e3b0967a8d84932316e9cbb2a2dcac1d8f204dc0
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-10 12:39:07 +0600

    made bestSkinDoctor part responsive for sm and md devices

--- src/components/SingleRatingCard/SingleRatingCard.jsx
+++ src/components/SingleRatingCard/SingleRatingCard.jsx
@@ -18,25 +18,25 @@ const SingleRatingCard = ({ doctor }) => {
   return (
     <div
       style={{ boxShadow: " 4px 4px 10px 0px rgba(0, 0, 0, 0.07)" }}
-      className="flex items-center gap-[26px] xl:gap-[16px] 2xl:gap-[34px] rounded-[40px] bg-white p-[11px] md:p-[14px] mt-[20px] mb-[40px] mx-[2px]"
+      className="flex items-center gap-[12px] lg:gap-[26px] xl:gap-[16px] 2xl:gap-[34px] rounded-[40px] bg-white p-[11px] md:p-[14px] mt-[20px] mb-[40px] mx-[2px]"
     >
       <div>
         <img
-          className="rounded-[40px] w-[111px] lg:w-[130px] xl:w-[120px] 2xl:w-auto"
+          className="rounded-[28px] lg:rounded-[40px] w-[96px] lg:w-[130px] xl:w-[120px] 2xl:w-auto"
           src={image}
           alt=""
         />
       </div>
       <div>
         {/* text */}
-        <div className="mb-[14px] xl:mb-[8px] 2xl:mb-[14px]">
-          <h5 className="text-black font-poppins text-base lg:text-base xl:text-base 2xl:text-xl font-semibold">
+        <div className="mb-[8px] xl:mb-[8px] 2xl:mb-[14px]">
+          <h5 className="text-black font-poppins text-xs lg:text-base xl:text-base 2xl:text-xl font-semibold">
             {name}
           </h5>
-          <h6 className="text-black font-poppins text-[17px] lg:text-base xl:text-base 2xl:text-xl font-light">
+          <h6 className="text-black font-poppins text-[13px] lg:text-base xl:text-base 2xl:text-xl font-light">
             {degree}
           </h6>
-          <p className="text-[#7F7F7F] font-poppins text-[15px] lg:text-base xl:text-base 2xl:text-xl">
+          <p className="text-[#7F7F7F] font-poppins text-[13px] lg:text-base xl:text-base 2xl:text-xl">
             {`${yearsOfExperience} Years of Experience`}
           </p>
         </div>
@@ -48,17 +48,17 @@ const SingleRatingCard = ({ doctor }) => {
             emptySymbol={
               <img
                 src={emptyStar}
-                className="icon w-[15px] lg:w-auto xl:w-[16px] 2xl:w-auto"
+                className="icon w-[13px] lg:w-auto xl:w-[16px] 2xl:w-auto"
               />
             }
             fullSymbol={
               <img
                 src={fullStar}
-                className="icon w-[15px] lg:w-auto xl:w-[16px] 2xl:w-auto"
+                className="icon w-[13px] lg:w-auto xl:w-[16px] 2xl:w-auto"
               />
             }
           />
-          <p className="text-black font-poppins text-sm font-semibold ml-[12px]">
+          <p className="text-black font-poppins text-xs font-semibold ml-[12px]">
             5 ({fiveStarRatingCount})
           </p>
         </div>
