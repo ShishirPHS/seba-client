9356b60eca3f5218887dd6e444f6c3be24bbd029
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-18 18:46:24 +0600

    created error page

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+const ErrorPage = () => {
+  return (
+    <div className="h-[100vh] bg-[#086060] py-[80px]">
+      <div className="flex flex-col items-center justify-center gap-[100px]">
+        <p className="text-center font-poppins text-lg max-w-[815px] text-white">
+          Sorry, we could not find the page you are looking for. Please check to
+          make sure you have typed the URL correctly.
+        </p>
+        <Link to="/" className="btn font-inter">
+          <button>Back to Home</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root/Root";
 import Home from "../pages/Home/Home";
 import Doctors from "../pages/Doctors/Doctors";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: "/",
