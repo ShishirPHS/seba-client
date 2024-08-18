62765492e894b86c42947aa606d92cc6e41e0a98
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-18 18:29:28 +0600

    height of 'menuList' is set

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,13 +1,11 @@
 import bannerImg from "../../../assets/banner/banner-image.png";
 import "./Banner.css";
 import locationIcon from "../../../assets/icons/location-icon.svg";
-import angelDown from "../../../assets/icons/angel-down.svg";
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import Select from "react-select";
 
 const Banner = () => {
-  const selectRef1 = useRef(null);
-  const selectRef2 = useRef(null);
+  const [menuListHeight, setMenuListHeight] = useState("120px");
 
   const symptoms = [
     { label: "Fever", value: "fever" },
@@ -117,31 +115,53 @@ const Banner = () => {
     },
   ];
 
+  const getHeightByScreenSize = (width) => {
+    if (width >= 1920) return "300px"; // 3xl
+    if (width >= 1589) return "280px"; // 2xl
+    if (width >= 1280) return "240px"; // xl
+    if (width >= 1024) return "140px"; // lg
+    if (width >= 768) return "120px"; // md
+    if (width >= 640) return "120px"; // sm
+    return "120px"; // default height for smaller screens
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      setMenuListHeight(getHeightByScreenSize(width));
+    };
+
+    handleResize(); // Set initial height
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // style for react-select
   const style = {
     control: (base) => ({
       ...base,
       border: 0,
       boxShadow: "none",
     }),
-  };
-
-  const handleImageClick = (ref) => {
-    if (ref.current) {
-      ref.current.focus();
-      ref.current.click();
-    }
+    menuList: (base) => ({
+      ...base,
+      height: menuListHeight,
+    }),
   };
 
   return (
     <div className="bg-[#086060] banner px-[24px] sm:px-0">
       <div className="container mx-auto relative overflow-hidden">
-        <div className="flex sm:mx-5">
+        <div className="sm:mx-5">
           {/* banner left */}
           <div className="mb-[70px] lg:mb-[70px] xl:mb-[130px] 2xl:mb-[156px] bg-white rounded-[40px] lg:rounded-[30px] xl:rounded-[40px] pt-[23px] lg:pt-[30px] xl:pt-[39px] 2xl:pt-[49px] pb-[23px] lg:pb-[25px] xl:pb-[40px] px-7 lg:px-[25px] xl:px-[44px] 2xl:px-16 mt-[64px] lg:mt-[80px] xl:mt-[150px] 2xl:mt-[177px] w-full md:w-[50%] relative z-20">
             <h3 className="mb-[25px] lg:mb-[30px] xl:mb-[50px] 2xl:mb-[62px] font-poppins text-xl sm:text-2xl lg:text-[30px] xl:text-[38px] 2xl:text-5xl text-center md:text-left font-bold max-w-[226px] sm:max-w-[270px] lg:max-w-[320px] xl:max-w-[412px] 2xl:max-w-[490px] leading-[123%] block md:inline-block mx-auto">
               Find the Best Doctor Near You
             </h3>
-            <form className="border border-[#808080] rounded-[25px] lg:rounded-[30px] xl:rounded-[40px] overflow-hidden">
+            <form className="border border-[#808080] rounded-[25px] lg:rounded-[30px] xl:rounded-[40px]">
               <div className="border-b border-[#808080] relative pt-[10px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[48px] px-[8px] lg:px-[12px] xl:px-[32px] pb-[2px] lg:pb-[6px] xl:pb-[8px] 2xl:pb-[20px]">
                 <input
                   type="text"
@@ -158,34 +178,20 @@ const Banner = () => {
               <div className="border-b border-[#808080] flex justify-between pt-[8px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[48px] px-[8px] lg:px-[12px] xl:px-[32px] pb-[2px] lg:pb-[6px] xl:pb-[20px] 2xl:pb-[30px] gap-[5px] lg:gap-[10px] xl:gap-[24px] 2xl:gap-[100px]">
                 <div className="w-full relative">
                   <Select
-                    ref={selectRef1}
                     className="w-full text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
                     options={symptoms}
                     placeholder="Symptoms"
                     noOptionsMessage={() => "No symptom found"}
                     styles={style}
                   />
-                  <img
-                    className="hidden right-[15px] lg:right-[12px] xl:right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer z-20 w-[19px] lg:w-[26px] xl:w-auto h-[27px] md:h-auto"
-                    src={angelDown}
-                    alt="angel down"
-                    onClick={() => handleImageClick(selectRef1)}
-                  />
                 </div>
                 <div className="w-full relative">
                   <Select
-                    ref={selectRef2}
                     className="w-full text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
                     options={symptoms}
                     placeholder="Diseases"
                     noOptionsMessage={() => "No disease found"}
                     styles={style}
-                  />
-                  <img
-                    className="hidden right-[15px] lg:right-[12px] xl:right-0 top-[50%] transform translate-y-[-50%] bg-white cursor-pointer z-20 w-[19px] lg:w-[26px] xl:w-auto h-[27px] md:h-auto"
-                    src={angelDown}
-                    alt="angel down"
-                    onClick={() => handleImageClick(selectRef2)}
                   />
                 </div>
               </div>
