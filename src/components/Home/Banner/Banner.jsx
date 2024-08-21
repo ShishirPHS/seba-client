61ef1d9661c6c169f316ee63ee59a664a94ce1fa
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-21 13:16:43 +0600

    improved responsiveness for banner part

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -178,28 +178,28 @@ const Banner = () => {
       <div className="container mx-auto relative overflow-hidden">
         <div className="sm:mx-5">
           {/* banner left */}
-          <div className="mb-[70px] lg:mb-[70px] xl:mb-[130px] 2xl:mb-[156px] bg-white rounded-[40px] lg:rounded-[30px] xl:rounded-[40px] pt-[23px] lg:pt-[30px] xl:pt-[39px] 2xl:pt-[49px] pb-[23px] lg:pb-[25px] xl:pb-[40px] px-7 lg:px-[25px] xl:px-[44px] 2xl:px-16 mt-[64px] lg:mt-[80px] xl:mt-[150px] 2xl:mt-[177px] w-full md:w-[50%] relative z-20">
+          <div className="mb-[70px] lg:mb-[70px] xl:mb-[130px] 2xl:mb-[156px] bg-white rounded-[40px] lg:rounded-[30px] xl:rounded-[40px] pt-[23px] lg:pt-[30px] xl:pt-[39px] 2xl:pt-[49px] pb-[23px] lg:pb-[25px] xl:pb-[40px] px-6 sm:px-7 lg:px-[25px] xl:px-[44px] 2xl:px-16 mt-[64px] lg:mt-[80px] xl:mt-[150px] 2xl:mt-[177px] w-full md:w-[50%] relative z-20">
             <h3 className="mb-[25px] lg:mb-[30px] xl:mb-[50px] 2xl:mb-[62px] font-poppins text-[20px] sm:text-[24px] lg:text-[30px] xl:text-[38px] 2xl:text-[48px] text-center md:text-left font-bold max-w-[226px] sm:max-w-[270px] lg:max-w-[320px] xl:max-w-[412px] 2xl:max-w-[490px] leading-[123%] block md:inline-block mx-auto">
               Find the Best Doctor Near You
             </h3>
             <form className="border border-[#808080] rounded-[25px] lg:rounded-[30px] xl:rounded-[40px]">
-              <div className="border-b border-[#808080] relative pt-[10px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[48px] px-[8px] lg:px-[12px] xl:px-[32px] pb-[2px] lg:pb-[6px] xl:pb-[8px] 2xl:pb-[20px]">
+              <div className="border-b border-[#808080] relative pt-[10px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[48px] pr-[8px] sm:px-2 lg:px-[12px] xl:px-[32px] pb-[2px] lg:pb-[6px] xl:pb-[8px] 2xl:pb-[20px]">
                 <input
                   type="text"
                   placeholder="Enter Your City or Location"
-                  className="input placeholder:font-poppins placeholder:text-sm lg:placeholder:text-lg xl:placeholder:text-2xl text-sm lg:text-lg xl:text-2xl flex-grow placeholder:text-[#808080] w-full"
+                  className="input placeholder:font-poppins placeholder:text-xs sm:placeholder:text-sm lg:placeholder:text-lg xl:placeholder:text-2xl text-sm lg:text-lg xl:text-2xl flex-grow placeholder:text-[#808080] w-full rounded-[20px]"
                   required
                 />
                 <img
-                  className="w-[19px] lg:w-[24px] h-[19px] md:h-[33px] absolute right-[25px] lg:right-[28px] xl:right-[40px] 2xl:right-[55px] top-[40%] lg:top-[50%] xl:top-[52%] 2xl:top-[60%] lg:transform lg:-translate-y-[42%] cursor-pointer"
+                  className="w-[19px] lg:w-[24px] h-[19px] md:h-[33px] absolute right-[16px] sm:right-[25px] lg:right-[28px] xl:right-[40px] 2xl:right-[55px] top-[40%] lg:top-[50%] xl:top-[52%] 2xl:top-[60%] lg:transform lg:-translate-y-[42%] cursor-pointer"
                   src={locationIcon}
                   alt="a icon of location"
                 />
               </div>
-              <div className="border-b border-[#808080] flex justify-between pt-[8px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[48px] px-[8px] lg:px-[12px] xl:px-[32px] pb-[2px] lg:pb-[6px] xl:pb-[20px] 2xl:pb-[30px] gap-[5px] lg:gap-[10px] xl:gap-[24px] 2xl:gap-[100px]">
+              <div className="border-b border-[#808080] flex justify-between sm:pt-[8px] lg:pt-[10px] xl:pt-[20px] 2xl:pt-[38px] pr-[16px] sm:pl-[8px] sm:pr-[25px] lg:pl-[12px] lg:pr-[28px] xl:pl-[32px] xl:pr-[40px] 2xl:pr-[55px] pb-[2px] lg:pb-[6px] xl:pb-[20px] 2xl:pb-[30px] gap-[5px] lg:gap-[10px] xl:gap-[24px] 2xl:gap-[100px]">
                 <div className="w-full relative">
                   <Select
-                    className="w-full text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
+                    className="w-full text-xs sm:text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
                     options={symptoms}
                     placeholder="Symptoms"
                     noOptionsMessage={() => "No symptom found"}
@@ -209,7 +209,7 @@ const Banner = () => {
                 </div>
                 <div className="w-full relative">
                   <Select
-                    className="w-full text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
+                    className="w-full text-xs sm:text-sm font-poppins lg:text-lg xl:text-2xl text-[#808080] bg-white"
                     options={symptoms}
                     placeholder="Diseases"
                     noOptionsMessage={() => "No disease found"}
