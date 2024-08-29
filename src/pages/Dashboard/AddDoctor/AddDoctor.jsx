b43199451c3495db27132cf691aae62324893f52
shishir-ahmed <shishirahmed790@gmail.com>
2024-08-29 13:57:26 +0600

    added validation and sweetAlert2

--- src/pages/Dashboard/AddDoctor/AddDoctor.jsx
+++ src/pages/Dashboard/AddDoctor/AddDoctor.jsx
@@ -1,30 +1,75 @@
+import Swal from "sweetalert2";
 import Container from "../../../components/shared/Container/Container";
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 const AddDoctor = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    trigger,
+  } = useForm();
 
-  const onFormSubmit = (data) => console.log(data);
+  const emailValue = watch("email");
+
+  useEffect(() => {
+    if (emailValue) {
+      trigger("email");
+    }
+  }, [emailValue, trigger]);
+
+  const onFormSubmit = (data) => {
+    console.log(data);
+
+    Swal.fire({
+      icon: "success",
+      title: "Doctor Added Successfully!",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  const registerOptions = {
+    name: { required: "Name is required" },
+    email: {
+      required: "Email is required",
+      pattern: {
+        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        message: "invalid email address",
+      },
+    },
+  };
 
   return (
     <div>
       <Container>
         <div className="3xl:mx-[140px]">
           <form onSubmit={handleSubmit(onFormSubmit)}>
-            <div className="space-x-4 mb-5">
+            {/* name */}
+            <div className="mb-5">
               <label htmlFor="name">Name</label>
               <input
-                name="name"
+                type="text"
                 className="border p-4 rounded-lg mr-4"
-                {...register("name", { required: true })}
+                {...register("name", registerOptions.name)}
               />
+              <p className="ml-0 text-red-500">
+                {errors?.name && errors.name.message}
+              </p>
             </div>
-            <div className="space-x-4 mb-5">
+            {/* email */}
+            <div className="mb-5">
               <label htmlFor="email">Email</label>
               <input
+                type="email"
                 className="border p-4 rounded-lg mr-4"
-                {...register("email", { required: true })}
+                {...register("email", registerOptions.email)}
               />
+              <p className="ml-0 text-red-500">
+                {errors?.email && errors.email.message}
+              </p>
             </div>
 
             <button type="submit" className="btn">
