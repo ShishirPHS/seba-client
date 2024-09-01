98ee3ec50c6779e03707c98ffb69279cc3ac1844
shishir-ahmed <shishirahmed790@gmail.com>
2024-09-01 14:33:37 +0600

    improved Add Doctor form

--- src/pages/Dashboard/AddDoctor/AddDoctor.jsx
+++ src/pages/Dashboard/AddDoctor/AddDoctor.jsx
@@ -133,205 +133,241 @@ const AddDoctor = () => {
     }
   };
   const registerOptions = {
-    doctorsName: { required: "Doctor's Name is required" },
+    doctorsName: { required: "Please enter doctor's name" },
     doctorsEmail: {
-      required: "Doctor's Email is required",
+      required: "Please enter doctor's email",
       pattern: {
         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-        message: "invalid email address",
+        message: "Invalid email address",
       },
     },
-    qualifications: { required: "Qualifications is required" },
-    specialty: { required: "Specialty is required" },
-    workplace: { required: "Workplace is required" },
+    qualifications: {
+      required: "Please enter your degrees and qualifications",
+    },
+    specialty: { required: "Please enter specialty" },
+    workplace: { required: "Please enter workplace" },
     designationAndDepartment: {
-      required: "Designation & Department is required",
+      required: "Please enter designation & department",
     },
     chamberName: {
-      required: "Chamber Name is required",
+      required: "Please enter chamber name",
+    },
+    chamberAddress: {
+      required: "Please enter chamber address",
     },
     visitingHour: {
-      required: "Visiting Hour is required",
+      required: "Please enter visiting hour",
     },
     mobileNumber: {
-      required: "Mobile Number is required",
+      required: "Please enter mobile number",
     },
     photo: {
-      required: "Photo is required",
+      required: "Please select a photo",
     },
   };
 
   return (
-    <div className="my-[140px]">
+    <div className="my-[100px]">
       <Container>
-        <div className="3xl:mx-[140px]">
-          <form
-            onSubmit={handleSubmit(onFormSubmit)}
-            className="border p-7 rounded-3xl"
-          >
-            {/* doctor's name */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="doctorsName"
-                className="mb-4"
-              >{`Doctor's Name`}</label>
-              <input
-                type="text"
-                placeholder="Enter Doctor's Name"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("doctorsName", registerOptions.doctorsName)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.doctorsName && errors.doctorsName.message}
-              </p>
-            </div>
-            {/* doctor's email */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="doctorsEmail"
-                className="mb-4"
-              >{`Doctor's Email`}</label>
-              <input
-                type="text"
-                placeholder="Enter Doctor's Email"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("doctorsEmail", registerOptions.doctorsEmail)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.doctorsEmail && errors.doctorsEmail.message}
-              </p>
-            </div>
-            {/* qualifications  */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="qualifications"
-                className="mb-4"
-              >{`Qualifications`}</label>
-              <input
-                type="text"
-                placeholder="Enter Qualifications"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("qualifications", registerOptions.qualifications)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.qualifications && errors.qualifications.message}
-              </p>
-            </div>
-            {/* specialty   */}
-            <div className="mb-5 flex flex-col items-start">
-              <label htmlFor="specialty" className="mb-4">{`Specialty`}</label>
-              <input
-                type="text"
-                placeholder="Enter Specialty"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("specialty", registerOptions.specialty)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.specialty && errors.specialty.message}
-              </p>
-            </div>
-            {/* workplace   */}
-            <div className="mb-5 flex flex-col items-start">
-              <label htmlFor="workplace" className="mb-4">{`Workplace`}</label>
-              <input
-                type="text"
-                placeholder="Enter Workplace"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("workplace", registerOptions.workplace)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.workplace && errors.workplace.message}
-              </p>
-            </div>
-            {/* designation & department   */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="designationAndDepartment"
-                className="mb-4"
-              >{`Designation & Department`}</label>
-              <input
-                type="text"
-                placeholder="Enter Designation & Department"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register(
-                  "designationAndDepartment",
-                  registerOptions.designationAndDepartment
-                )}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.designationAndDepartment &&
-                  errors.designationAndDepartment.message}
-              </p>
-            </div>
-            {/* chamber name   */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="chamberName"
-                className="mb-4"
-              >{`Chamber Name`}</label>
-              <input
-                type="text"
-                placeholder="Enter Chamber Name"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("chamberName", registerOptions.chamberName)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.chamberName && errors.chamberName.message}
-              </p>
-            </div>
-            {/* visiting hour   */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="visitingHour"
-                className="mb-4"
-              >{`Visiting Hour`}</label>
-              <input
-                type="text"
-                placeholder="Enter Visiting Hour"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("visitingHour", registerOptions.visitingHour)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.visitingHour && errors.visitingHour.message}
-              </p>
-            </div>
-            {/* mobile number */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="mobileNumber"
-                className="mb-4"
-              >{`Mobile Number (For Appointment)`}</label>
-              <input
-                type="text"
-                placeholder="Enter Mobile Number"
-                className="border py-3 px-5 rounded-lg mr-4 w-full"
-                {...register("mobileNumber", registerOptions.mobileNumber)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.mobileNumber && errors.mobileNumber.message}
-              </p>
-            </div>
+        <div className="mx-5 3xl:mx-[140px]">
+          <div className="border p-7 rounded-3xl max-w-[1000px] mx-auto font-poppins">
+            <h4 className="text-center text-3xl font-bold mb-10">Add Doctor</h4>
+            <form onSubmit={handleSubmit(onFormSubmit)} className="">
+              {/* doctor's name */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="doctorsName" className="mb-2 font-medium">
+                  {`Doctor's Name`}
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter doctor's name"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("doctorsName", registerOptions.doctorsName)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.doctorsName && errors.doctorsName.message}
+                </p>
+              </div>
+              {/* doctor's email */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="doctorsEmail" className="mb-2 font-medium">
+                  {`Doctor's Email`}
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter doctor's valid email"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("doctorsEmail", registerOptions.doctorsEmail)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.doctorsEmail && errors.doctorsEmail.message}
+                </p>
+              </div>
+              {/* qualifications  */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="qualifications" className="mb-2 font-medium">
+                  Qualifications
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Enter your degrees and qualifications"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register(
+                    "qualifications",
+                    registerOptions.qualifications
+                  )}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.qualifications && errors.qualifications.message}
+                </p>
+              </div>
+              {/* specialty   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="specialty" className="mb-2 font-medium">
+                  Specialty
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Example: Medicine Specialist"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("specialty", registerOptions.specialty)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.specialty && errors.specialty.message}
+                </p>
+              </div>
+              {/* workplace   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="workplace" className="mb-2 font-medium">
+                  Workplace
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Example: Dhaka Medical College Hospital"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("workplace", registerOptions.workplace)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.workplace && errors.workplace.message}
+                </p>
+              </div>
+              {/* designation & department   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label
+                  htmlFor="designationAndDepartment"
+                  className="mb-2 font-medium"
+                >
+                  Designation & Department
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Example: Assistant Professor (Medicine)"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register(
+                    "designationAndDepartment",
+                    registerOptions.designationAndDepartment
+                  )}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.designationAndDepartment &&
+                    errors.designationAndDepartment.message}
+                </p>
+              </div>
+              {/* chamber name   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="chamberName" className="mb-2 font-medium">
+                  Chamber Name
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Example: Popular Diagnostic Centre, Dhanmondi"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("chamberName", registerOptions.chamberName)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.chamberName && errors.chamberName.message}
+                </p>
+              </div>
+              {/* chamber address   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="chamberAddress" className="mb-2 font-medium">
+                  Chamber Address
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="House #16 Road No. 2, Dhaka 1205"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register(
+                    "chamberAddress",
+                    registerOptions.chamberAddress
+                  )}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.chamberAddress && errors.chamberAddress.message}
+                </p>
+              </div>
+              {/* visiting hour   */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="visitingHour" className="mb-2 font-medium">
+                  Visiting Hour
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Example: 6pm to 9pm (Closed: Friday)"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("visitingHour", registerOptions.visitingHour)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.visitingHour && errors.visitingHour.message}
+                </p>
+              </div>
+              {/* mobile number */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="mobileNumber" className="mb-2 font-medium">
+                  Mobile Number (For Appointment)
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="017********"
+                  className="border py-3 px-5 rounded-lg mr-4 w-full"
+                  {...register("mobileNumber", registerOptions.mobileNumber)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.mobileNumber && errors.mobileNumber.message}
+                </p>
+              </div>
 
-            {/* file input */}
-            <div className="mb-5 flex flex-col items-start">
-              <label
-                htmlFor="photo"
-                className="mb-4"
-              >{`Upload Your Photo`}</label>
-              <input
-                type="file"
-                className="file-input file-input-bordered file-input-md w-full"
-                {...register("photo", registerOptions.photo)}
-              />
-              <p className="ml-0 text-red-500 mt-2">
-                {errors?.photo && errors.photo.message}
-              </p>
-            </div>
+              {/* file input */}
+              <div className="mb-3 flex flex-col items-start">
+                <label htmlFor="photo" className="mb-2 font-medium">
+                  Upload Your Photo
+                  <span className="text-red-600 font-normal ml-1">*</span>
+                </label>
+                <input
+                  type="file"
+                  className="file-input file-input-bordered file-input-md w-full"
+                  {...register("photo", registerOptions.photo)}
+                />
+                <p className="ml-0 text-red-500 mt-2">
+                  {errors?.photo && errors.photo.message}
+                </p>
+              </div>
 
-            <button type="submit" className="btn">
-              Add Doctor
-            </button>
-          </form>
+              <button type="submit" className="btn">
+                Add Doctor
+              </button>
+            </form>
+          </div>
         </div>
       </Container>
     </div>
--- src/pages/Doctors/Doctors.jsx
+++ src/pages/Doctors/Doctors.jsx
@@ -7,7 +7,7 @@ const Doctors = () => {
   console.log(allDoctors);
 
   return (
-    <div className="container mx-auto px-[140px] py-[100px]">
+    <div className="container mx-auto px-5 3xl:px-[140px] py-[100px]">
       <div className="grid grid-cols-3 gap-6">
         {allDoctors?.map((doctor) => (
           <DoctorCard key={doctor._id} doctor={doctor}></DoctorCard>
