cb8c10be4ddd016f708c22a2581eaec2d78a415c
shishir-ahmed <shishirahmed790@gmail.com>
2024-09-17 20:21:01 +0600

    dynamically rendered scrolling arrow btns

--- src/components/DoctorCard/DoctorCard.jsx
+++ src/components/DoctorCard/DoctorCard.jsx
@@ -2,10 +2,13 @@ import PropTypes from "prop-types";
 import "./DoctorCard.css";
 import ChamberCard from "./ChamberCard/ChamberCard";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const DoctorCard = ({ doctor }) => {
   const { doctorsName, specialty, mobileNumber, chamberInfos, photo } = doctor;
+
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(false);
 
   const specialties = specialty
     .map((singleSpecialty) => singleSpecialty.label)
@@ -13,16 +16,46 @@ const DoctorCard = ({ doctor }) => {
 
   const containerRef = useRef(null);
 
+  // Check if content overflows and enable/disable arrows accordingly
+  const checkScrollAbility = () => {
+    const container = containerRef.current;
+    if (container) {
+      setCanScrollLeft(container.scrollLeft > 0);
+      setCanScrollRight(
+        container.scrollWidth > container.clientWidth &&
+          container.scrollLeft < container.scrollWidth - container.clientWidth
+      );
+    }
+  };
+
+  useEffect(() => {
+    checkScrollAbility();
+    // Add an event listener to detect changes in scroll
+    const container = containerRef.current;
+    if (container) {
+      container.addEventListener("scroll", checkScrollAbility);
+    }
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      if (container) {
+        container.removeEventListener("scroll", checkScrollAbility);
+      }
+    };
+  }, [doctor]);
+
   const scrollLeft = () => {
     if (containerRef.current) {
       containerRef.current.scrollLeft -= 450;
     }
+    checkScrollAbility();
   };
 
   const scrollRight = () => {
     if (containerRef.current) {
       containerRef.current.scrollLeft += 450;
     }
+    checkScrollAbility();
   };
 
   return (
@@ -53,14 +86,17 @@ const DoctorCard = ({ doctor }) => {
       <div></div>
       {/* bottom div to show chamber infos */}
       <div className="relative">
-        <FaChevronLeft
-          style={{
-            boxSizing: "content-box",
-            boxShadow: "0px 4px 2px rgba(0, 0, 0, 0.25)",
-          }}
-          className="hidden lg:block absolute top-[50%] translate-y-[-50%] left-[-10px] p-[5px] rounded-full bg-white cursor-pointer"
-          onClick={scrollLeft}
-        />
+        {canScrollLeft && (
+          <FaChevronLeft
+            style={{
+              boxSizing: "content-box",
+              boxShadow: "0px 4px 2px rgba(0, 0, 0, 0.25)",
+            }}
+            className="hidden lg:block absolute top-[50%] translate-y-[-50%] left-[-10px] p-[5px] rounded-full bg-white cursor-pointer"
+            onClick={scrollLeft}
+          />
+        )}
+
         <div
           className={`mt-5 flex overflow-x-auto gap-6 scrollbar-hide scroll-smooth mx-0 lg:mx-3`}
           ref={containerRef}
@@ -72,14 +108,16 @@ const DoctorCard = ({ doctor }) => {
             ></ChamberCard>
           ))}
         </div>
-        <FaChevronRight
-          style={{
-            boxSizing: "content-box",
-            boxShadow: "0px 4px 2px rgba(0, 0, 0, 0.25)",
-          }}
-          className="hidden lg:block absolute top-[50%] translate-y-[-50%] right-[-10px] p-[5px] rounded-full bg-white cursor-pointer"
-          onClick={scrollRight}
-        />
+        {canScrollRight && (
+          <FaChevronRight
+            style={{
+              boxSizing: "content-box",
+              boxShadow: "0px 4px 2px rgba(0, 0, 0, 0.25)",
+            }}
+            className="hidden lg:block absolute top-[50%] translate-y-[-50%] right-[-10px] p-[5px] rounded-full bg-white cursor-pointer"
+            onClick={scrollRight}
+          />
+        )}
       </div>
     </div>
   );
